4203b403fc29c410dc8ab065af055caeb067bfb1
magayton <mathis.gayton@gmail.com>
2025-06-16 17:01:20 +0200

    Add deployment script

--- Payment/ton-payment/scripts/deployPayment.ts
+++ Payment/ton-payment/scripts/deployPayment.ts
@@ -1,19 +1,25 @@
 import { toNano } from '@ton/core';
-import { Payment } from '../build/Payment/Payment_Payment';
+import { TONPaymentHandler } from '../build/TONPaymentHandler/TONPaymentHandler_TONPaymentHandler';
 import { NetworkProvider } from '@ton/blueprint';
 
 export async function run(provider: NetworkProvider) {
-    const payment = provider.open(await Payment.fromInit());
+    console.log('Deploying TON Payment Handler...');
+    
+    // No param needed since contract use "sender()" as owner
+    const payment = provider.open(
+        await TONPaymentHandler.fromInit()
+    );
 
+    console.log('Contract address:', payment.address.toString());
+
+    // Use "send" method to deploy
+    // https://docs.tact-lang.org/book/gas-best-practices/#do-not-deploy-contracts-with-deployable-trait
     await payment.send(
         provider.sender(),
-        {
-            value: toNano('0.05'),
-        },
-        null,
+        { value: toNano('0.1') },
+        null
     );
 
     await provider.waitForDeploy(payment.address);
-
-    // run methods on `payment`
+    console.log('✅ Payment contract deployed!');
 }
